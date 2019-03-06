2ce92e3248b237114cb24276b3fa236a4b8fd91d
poilpy <colinmurphy97@gmail.com>
2019-03-05 22:47:29 -0800

    fcn for seg, slow as hell

--- test_model.py
+++ test_model.py
@@ -28,7 +28,7 @@ def main():
 	print("Import success!")
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 	model = models.resnet101(pretrained=True)
-	data_dir = './data/train'
+	data_dir = 'data/cifar-10-batches-py/'
 	transform = transforms.Compose(
 		[transforms.ToTensor(),
 		 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -36,17 +36,20 @@ def main():
 	trainloader, testloader = load_split_train_test(data_dir, .2)
 	#print(trainloader.dataset.classes)
 
-	trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-											download=True, transform=transform)
-	trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-											  shuffle=True, num_workers=2)
+	# trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+	# 										download=True, transform=transform)
+	# trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+	# 										  shuffle=True, num_workers=2)
 
-	testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-										   download=True, transform=transform)
-	testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-											 shuffle=False, num_workers=2)
+	# testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+	# 									   download=True, transform=transform)
+	# testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+	# 										 shuffle=False, num_workers=2)
 
 	print(model)
+	print('----------------------')
+	print(trainloader.dataset)
+
 	for param in model.parameters():
 		param.requires_grad = False
 	
@@ -55,45 +58,45 @@ def main():
 	optimizer = optim.Adam(model.fc.parameters(), lr=0.003)
 	model.to(device)
 
-	epochs = 2
-	steps = 0
-	running_loss = 0
-	print_every = 10
-	train_losses, test_losses = [], []
-	for epoch in range(epochs):
-		for inputs, labels in trainloader:
-			steps += 1
-			inputs, labels = inputs.to(device), labels.to(device)
-			optimizer.zero_grad()
-			logps = model.forward(inputs)
-			loss = criterion(logps, labels)
-			loss.backward()
-			optimizer.step()
-			running_loss += loss.item()
+	# epochs = 2
+	# steps = 0
+	# running_loss = 0
+	# print_every = 10
+	# train_losses, test_losses = [], []
+	# for epoch in range(epochs):
+	# 	for inputs, labels in trainloader:
+	# 		steps += 1
+	# 		inputs, labels = inputs.to(device), labels.to(device)
+	# 		optimizer.zero_grad()
+	# 		logps = model.forward(inputs)
+	# 		loss = criterion(logps, labels)
+	# 		loss.backward()
+	# 		optimizer.step()
+	# 		running_loss += loss.item()
 			
-			if steps % print_every == 0:
-				test_loss = 0
-				accuracy = 0
-				model.eval()
-				with torch.no_grad():
-					for inputs, labels in testloader:
-						inputs, labels = inputs.to(device),labels.to(device)
-						logps = model.forward(inputs)
-						batch_loss = criterion(logps, labels)
-						test_loss += batch_loss.item()
+	# 		if steps % print_every == 0:
+	# 			test_loss = 0
+	# 			accuracy = 0
+	# 			model.eval()
+	# 			with torch.no_grad():
+	# 				for inputs, labels in testloader:
+	# 					inputs, labels = inputs.to(device),labels.to(device)
+	# 					logps = model.forward(inputs)
+	# 					batch_loss = criterion(logps, labels)
+	# 					test_loss += batch_loss.item()
 						
-						ps = torch.exp(logps)
-						top_p, top_class = ps.topk(1, dim=1)
-						equals = top_class == labels.view(*top_class.shape)
-						accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
-				train_losses.append(running_loss/len(trainloader))
-				test_losses.append(test_loss/len(testloader))					
-				print(f"Epoch {epoch+1}/{epochs}.. "
-					  f"Train loss: {running_loss/print_every:.3f}.. "
-					  f"Test loss: {test_loss/len(testloader):.3f}.. "
-					  f"Test accuracy: {accuracy/len(testloader):.3f}")
-				running_loss = 0
-				model.train()
+	# 					ps = torch.exp(logps)
+	# 					top_p, top_class = ps.topk(1, dim=1)
+	# 					equals = top_class == labels.view(*top_class.shape)
+	# 					accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+	# 			train_losses.append(running_loss/len(trainloader))
+	# 			test_losses.append(test_loss/len(testloader))					
+	# 			print(f"Epoch {epoch+1}/{epochs}.. "
+	# 				  f"Train loss: {running_loss/print_every:.3f}.. "
+	# 				  f"Test loss: {test_loss/len(testloader):.3f}.. "
+	# 				  f"Test accuracy: {accuracy/len(testloader):.3f}")
+	# 			running_loss = 0
+	# 			model.train()
 
 	print('Finished Training')
 
---
+++ transforms.py
@@ -0,0 +1,104 @@
+import random
+
+import numpy as np
+# from skimage.filters import gaussian
+import torch
+from PIL import Image, ImageFilter
+    
+import os
+from math import ceil
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torch.autograd import Variable
+
+class RandomVerticalFlip(object):
+    def __call__(self, img):
+        if random.random() < 0.5:
+            return img.transpose(Image.FLIP_TOP_BOTTOM)
+        return img
+
+
+class DeNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.mul_(s).add_(m)
+        return tensor
+
+
+class MaskToTensor(object):
+    def __call__(self, img):
+        return torch.from_numpy(np.array(img, dtype=np.int32)).long()
+
+
+class FreeScale(object):
+    def __init__(self, size, interpolation=Image.BILINEAR):
+        self.size = tuple(reversed(size))  # size: (h, w)
+        self.interpolation = interpolation
+
+    def __call__(self, img):
+        return img.resize(self.size, self.interpolation)
+
+
+class FlipChannels(object):
+    def __call__(self, img):
+        img = np.array(img)[:, :, ::-1]
+        return Image.fromarray(img.astype(np.uint8))
+
+def check_mkdir(dir_name):
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+
+def evaluate(predictions, gts, num_classes):
+    hist = np.zeros((num_classes, num_classes))
+    for lp, lt in zip(predictions, gts):
+        hist += _fast_hist(lp.flatten(), lt.flatten(), num_classes)
+    # axis 0: gt, axis 1: prediction
+    acc = np.diag(hist).sum() / hist.sum()
+    acc_cls = np.diag(hist) / hist.sum(axis=1)
+    acc_cls = np.nanmean(acc_cls)
+    iu = np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist))
+    mean_iu = np.nanmean(iu)
+    freq = hist.sum(axis=1) / hist.sum()
+    fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
+    return acc, acc_cls, mean_iu, fwavacc
+
+
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+class CrossEntropyLoss2d(nn.Module):
+    def __init__(self, weight=None, size_average=True, ignore_index=255):
+        super(CrossEntropyLoss2d, self).__init__()
+        self.nll_loss = nn.NLLLoss2d(weight, size_average, ignore_index)
+
+    def forward(self, inputs, targets):
+        return self.nll_loss(F.log_softmax(inputs), targets)
+
+
+# class RandomGaussianBlur(object):
+#     def __call__(self, img):
+#         sigma = 0.15 + random.random() * 1.15
+#         blurred_img = gaussian(np.array(img), sigma=sigma, multichannel=True)
+#         blurred_img *= 255
+#         return Image.fromarray(blurred_img.astype(np.uint8))
